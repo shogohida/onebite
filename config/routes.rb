429a4cdd779d1e8fd82456a5c76c46ee90b368d6
Shogo Hida <shogo.hida@gmail.com>
2020-05-25 20:06:49 +0900

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources: courses, only: [:index] do
-    resources: enrollments, only: [:new, :create]
+  resources :courses, only: :index do
+    resources :enrollments, only: [:new, :create]
   end
-  resources: enrollments, only: [:index, :show]
-  resources: users, only: :show
+  resources :enrollments, only: [:index, :show]
+  resources :users, only: :show
 end
