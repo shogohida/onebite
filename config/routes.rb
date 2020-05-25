e0c5c46cb97fca6920bdc0be2893cea435ceac87
Shogo Hida <shogo.hida@gmail.com>
2020-05-25 20:15:39 +0900

    created controllers but need to fix them

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'enrollments/index'
+  get 'enrollments/show'
+  get 'enrollments/new'
+  get 'courses/index'
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
-  resources: courses, only: [:index] do
-    resources: enrollments, only: [:new, :create]
+  resources :courses, only: [:index] do
+    resources :enrollments, only: [:new, :create]
   end
-  resources: enrollments, only: [:index, :show]
-  resources: users, only: :show
+  resources :enrollments, only: [:index, :show]
+  resources :users, only: :show
 end
---
+++ test/controllers/enrollments_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EnrollmentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get enrollments_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get enrollments_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get enrollments_new_url
+    assert_response :success
+  end
+
+end
