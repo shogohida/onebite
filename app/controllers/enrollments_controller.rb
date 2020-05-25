25de5f015da445fd6566a6547243a4ca9f1d9169
Shogo Hida <shogo.hida@gmail.com>
2020-05-25 22:17:22 +0900

    added pundit

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,24 +1,36 @@
 class EnrollmentsController < ApplicationController
   def index
-    @enrollments = Enrollment.all
+    @enrollments = policy_scope(Enrollment).all
     # policy scope with Pundit installed
   end
 
   def show
     @enrollment = Enrollment.find(params[:id])
+    authorize @enrollment
   end
 
   def new
     @enrollment = Enrollment.new
+    authorize @enrollment
   end
 
   def create
+    @course = Course.find(params[:course_id])
     @enrollment = Enrollment.new(enrollment_params)
+    @enrollment.course = @course
+    @enrollment.user = user
+    authorize @course
+    authorize @enrollment
+    if @enrollment.save
+      redirect_to enrollments_path
+    else
+      render :new
+    end
   end
 
   private
 
   def enrollment_params
-    params.require(:enrollment).permit()
+    params.require(:enrollment).permit(:course_id, :user_id, :start_date, :completed_at, :duration, :time_of_day, :completion_status)
   end
 end
