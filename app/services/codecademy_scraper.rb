edb42aa6e31faaa3c78dafb3ac5bd0071dcb9afd
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-27 22:18:55 +0900

    first lines of code for setting up the scraping on codecademy

---
+++ app/services/codecademy_scraper.rb
@@ -0,0 +1,28 @@
+class CodecademyScraper
+
+  def initialize(course_id)
+    @course_id = course_id
+  end
+
+  # require 'open-uri'
+  # require 'nokogiri'
+
+  def fetch_courses_urls
+    full_catalog_url = "https://www.codecademy.com/catalog/subject/all"
+    doc = Nokogiri::HTML(open(full_catalog_url).read)
+    courses = doc.search("#course a")
+    courses.map do |course|
+      uri = URI.parse(course.attributes["href"].value)
+      uri.scheme = "http"
+      uri.host = "www.codecademy.com"
+      uri.query = nil
+      uri.to_s
+    end
+  end
+
+  def perform_scraping
+
+  end
+
+
+end
