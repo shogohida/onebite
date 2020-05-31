b207be547d9810ef237dbce0f66d71d3d65d3f7e
Shogo Hida <shogo.hida@gmail.com>
2020-05-31 20:56:29 +0900

    need to access to user from chapter

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,18 +1,26 @@
 class ChaptersController < ApplicationController
   def update
     @chapter = Chapter.find(params[:id])
+    # @user = User.find(params[:user_id])?
+    # @enrollment = Enrollment.find(params[:enrollment_id])?
+    # @course = Course.find(params[:course_id])?
     @chapter.update(chapter_params)
+    #@chapter.save
     authorize @chapter
+    # raise
     # if @chapter.save
-
+    #   redirect_to root_path
+    #   current_user.streak += 1 (@user?)
+    #   @chapter.course.enrollments....
+    #   @enrollment.completion_status = (@enrollment.completion_status * @course.chapters.count + 1) / @course.chapters.count
     # else
-    #  render :
+    #   redirect_to courses_path
     # end
   end
 
   private
 
   def chapter_params
-    params.require(:chapter).permit(:done)
+    params.require(:chapter).permit(:done, :id, :name, :course_id)
   end
 end
