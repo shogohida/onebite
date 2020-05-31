fa9c78bc2c70bde3188de7c047d69ed885b55cc8
Shogo Hida <shogo.hida@gmail.com>
2020-05-31 17:00:57 +0900

    incomplete form for done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_012518) do
+ActiveRecord::Schema.define(version: 2020_05_31_073114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_012518) do
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "done", default: false
     t.index ["course_id"], name: "index_chapters_on_course_id"
   end
 
