efb4d63438327bf6aa2b88f4395d402e65722f19
Shogo Hida <shogo.hida@gmail.com>
2020-06-03 17:02:21 +0900

    added sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -24,6 +28,8 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+
 
   private
 
