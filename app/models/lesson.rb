c2dff5b1459609a1105467f23dfe59846115f205
Shogo Hida <shogohida@users.noreply.github.com>
2020-08-31 00:02:50 +0900

    Update lesson.rb

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,5 @@
 class Lesson < ApplicationRecord
-  belongs_to :chapter
-  # validates :name, presence:true
+  belongs_to :chapte
   def start_time
     self.chapter.course.enrollments.last.start_date.start ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
   end
