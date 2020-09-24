ce60873df9b663565e30688ce5aa26366e339123
Shogo Hida <shogo.hida@gmail.com>
2020-09-25 01:15:55 +0900

    added fasterer

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_back(fallback_location: root_path), notice: "Logged in as a test user"
+    # redirect_back(fallback_location: root_path), notice: "Logged in as a test user"
   end
 end
