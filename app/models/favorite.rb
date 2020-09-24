1e6553c95383c8c7e2ae282e4f9325e86e864885
Shogo Hida <shogo.hida@gmail.com>
2020-09-24 23:43:13 +0900

    delegated favoritable to favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,7 @@ class Favorite < ApplicationRecord
   extend ActsAsFavoritor::FavoriteScopes
 
   belongs_to :favoritable, polymorphic: true
+  delegate :name, :courses, :to => :favoritable, :prefix => true
   belongs_to :favoritor, polymorphic: true
 
   # def block!
